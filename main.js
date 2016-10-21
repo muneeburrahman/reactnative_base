5ce088878bd602724b9077b2a5d664ae5969d467
AsimAshfaq <asimizb4@gmail.com>
2016-10-21 16:00:49 +0500

    Index.js 100% eslint clean format

--- main.js
+++ main.js
@@ -7,34 +7,34 @@ import {
   View,
 } from 'react-native';
 
-import TaskList from './TaskList'
+import TaskList from './TaskList';
 class PluralTodo extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      todos:[
-        {
-          task: 'Learn React Native',
-        }
-      ],
-    };
-  }
-  render() {
-    return (
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            todos: [
+                {
+                    task: 'Learn React Native',
+                },
+            ],
+        };
+    }
+    render() {
+        return (
       <View style={styles.container}>
         <TaskList />
       </View>
     );
-  }
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 
 Exponent.registerRootComponent(PluralTodo);
